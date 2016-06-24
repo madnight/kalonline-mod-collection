c0ab42e5f1639f387c41c097048c01109ad3c9ba
Fabian Beuke <fabianbeuke@googlemail.com>
2016-06-24 14:17:34 +0200

    add GPLv2

--- MadEvent/MadEvent.h
+++ MadEvent/MadEvent.h
@@ -1,3 +1,16 @@
+/*
+ * (C) 2016 Fabian Beuke <mail@beuke.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+ 
 #include <Windows.h>
 #include "Utilities.h"
 
