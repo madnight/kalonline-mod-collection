40bb55643801fb971b16401d546313c86d9c6472
Fabian Beuke <mail@beuke.org>
2020-04-21 23:16:31 +0200

    some core refactoring

--- Core/AmplificationOfBlood.h
+++ Core/AmplificationOfBlood.h
@@ -12,38 +12,44 @@ void __fastcall AmplificationOfBlood(IChar IPlayer, int pPacket, int pPos)
         CPacket::Read((char*)pPacket, (char*)pPos, "bd", &bType, &nTargetID);
         int nMana = (106 + (nSkillGrade * 2));
 
-        if (bType == 0 && nTargetID)
+        if (bType == 0 && nTargetID) {
             pTarget = CPlayer::FindPlayer(nTargetID);
+        }
 
-        if (bType == 1 && nTargetID)
+        if (bType == 1 && nTargetID) {
             pTarget = CMonster::FindMonster(nTargetID);
+        }
 
-        if (bType >= 2)
+        if (bType >= 2) {
             return;
+        }
 
         if (pTarget && nSkillGrade && IPlayer.IsValid())
         {
             IChar Target(pTarget);
 
-            if (IPlayer.GetCurMp() < nMana)
+            if (IPlayer.GetCurMp() < nMana || pTarget == IPlayer.GetOffset()) {
                 return;
+            }
 
-            if (pTarget == IPlayer.GetOffset())
-                return;
-
-            if(IPlayer.IsValid() && Target.IsValid())
+            if (IPlayer.IsValid() && Target.IsValid())
             {
                 if (!IPlayer.IsInRange(Target,300))
                     return;
 
                 int XpSkill = IPlayer.GetSkillPointer(41);
+                int decHpVal = (IPlayer.GetCurHp() * 5) / 100;
 
                 if (XpSkill)
                 {
                     ISkill xSkillx((void*)XpSkill);
-                    if (xSkillx.GetGrade() <= CTools::Rate(0,6)) IPlayer.DecreaseHp((IPlayer.GetCurHp() * 5) /100);
+
+                    if (xSkillx.GetGrade() <= CTools::Rate(0,6)) {
+                        IPlayer.DecreaseHp(decHpVal);
+                    }
+
                 } else {
-                    IPlayer.DecreaseHp((IPlayer.GetCurHp() * 5) /100);
+                    IPlayer.DecreaseHp(decHpVal);
                 }
 
                 int Around = Target.GetObjectListAround(1);
@@ -51,7 +57,11 @@ void __fastcall AmplificationOfBlood(IChar IPlayer, int pPacket, int pPos)
                                 (CChar::GetInt((int)IPlayer.GetOffset()) / 1)) +
                             ((CChar::GetWis((int)IPlayer.GetOffset()) / 1) +
                              (nSkillGrade * 350)))) + (IPlayer.GetLevel() * 10);
-                if(Target.GetType() == 0) nDmg = (nDmg * SHMAOB) / 100;
+
+                if (Target.GetType() == 0) {
+                    nDmg = (nDmg * SHMAOB) / 100;
+                }
+
                 IPlayer._ShowBattleAnimation(Target, 38);
                 IPlayer.SetDirection(Target);
                 IPlayer.DecreaseMana(nMana);
@@ -63,8 +73,9 @@ void __fastcall AmplificationOfBlood(IChar IPlayer, int pPacket, int pPos)
                     if (Object.IsValid() && IPlayer.IsValid() &&
                             (*(int (__thiscall **) (int, int, DWORD))
                              (*(DWORD *)IPlayer.GetOffset() + 176))
-                            ((int)IPlayer.GetOffset(), (int)Object.GetOffset(), 0))
+                            ((int)IPlayer.GetOffset(), (int)Object.GetOffset(), 0)) {
                         IPlayer.OktayDamageArea(Object,nDmg,38);
+                    }
 
                     Around = CBaseList::Pop((void*)Around);
                 }
--- Core/AnkleAmputate.h
+++ Core/AnkleAmputate.h
@@ -11,46 +11,56 @@ void __fastcall AnkleAmputate(IChar IPlayer, int pPacket, int pPos)
         void *pTarget = 0;
         CPacket::Read((char*)pPacket, (char*)pPos, "bd", &bType, &nTargetID);
 
-        if (bType == 0 && nTargetID)
+        if (bType == 0 && nTargetID) {
             pTarget = CPlayer::FindPlayer(nTargetID);
+        }
 
-        if (bType == 1 && nTargetID)
+        if (bType == 1 && nTargetID) {
             pTarget = CMonster::FindMonster(nTargetID);
+        }
 
-        if (bType >= 2)
+        if (bType >= 2) {
             return;
+        }
 
         if (IPlayer.IsValid() && pTarget && nSkillGrade)
         {
-            if (pTarget == IPlayer.GetOffset())
+            if (pTarget == IPlayer.GetOffset()) {
                 return;
+            }
 
             int nMana = (10 * nSkillGrade) + 45;
             IChar Target(pTarget);
 
-            if (IPlayer.GetCurMp() < nMana)
+            if (IPlayer.GetCurMp() < nMana) {
                 return;
+            }
 
             if (IPlayer.IsValid() && Target.IsValid())
             {
-                if (!CanAttack(IPlayer.GetOffset(),0,(int)Target.GetOffset(),0))
+                if (!CanAttack(IPlayer.GetOffset(), 0, (int)Target.GetOffset(), 0)) {
                     return;
+                }
 
-                if (!IPlayer.IsInRange(Target,300))
+                if (!IPlayer.IsInRange(Target, 300)) {
                     return;
+                }
 
                 IPlayer.DecreaseMana(nMana);
-                Target.Buff(40,nSkillGrade*3,-50);
 
-                if (!Target.IsBuff(346))
-                    Target.SendGStateEx(Target.GetGStateEx()+256);
+                Target.Buff(40, nSkillGrade * 3, -50);
 
-                if (Target.GetType() == 0)
-                    Target.RemoveBuffIcon(0,0,0,54);
+                if (!Target.IsBuff(346)) {
+                    Target.SendGStateEx(Target.GetGStateEx() + 256);
+                }
 
-                Target.Buff(346,nSkillGrade*3,0);
+                if (Target.GetType() == 0) {
+                    Target.RemoveBuffIcon(0, 0, 0, 54);
+                }
+
+                Target.Buff(346, nSkillGrade * 3, 0);
                 IPlayer.SetDirection(Target);
-                IPlayer._ShowBattleAnimation(Target,15,nSkillGrade);
+                IPlayer._ShowBattleAnimation(Target, 15, nSkillGrade);
             }
         }
     }
