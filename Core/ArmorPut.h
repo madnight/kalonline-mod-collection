f879a6136ddd6a0817ae2627b71cbdf694c6118c
Fabian Beuke <mail@beuke.org>
2020-05-04 17:57:34 +0200

    some refactoring

--- Core/ArmorPut.h
+++ Core/ArmorPut.h
@@ -3,6 +3,39 @@ int getItemStat(int x, int y, int item) {
     return (GetItemStat.find(IItem.GetIID())->second % x) / y;
 }
 
+void demonGongTypeApply(int type, int stat, int player, bool remove){
+    IChar IPlayer((void*)player);
+    int s = stat -1;
+    if (remove) {
+        switch (type)
+        {
+            case 1:
+                IPlayer.RemoveDef(DemonGongStatArmorEarth[0][s]);
+                IPlayer.RemoveOTP(DemonGongStatArmorEarth[1][s]);
+            case 2:
+                IPlayer.DecreaseMaxHp(DemonGongStatArmorWind[0][s]);
+                IPlayer.RemoveEva(DemonGongStatArmorWind[1][s]);
+            case 3:
+                IPlayer.RemoveAgi(DemonGongStatArmorWater[0][s]);
+                IPlayer.RemoveDef(DemonGongStatArmorWater[1][s]);
+        }
+    } else {
+        switch (type)
+        {
+            case 1:
+                IPlayer.AddDef(DemonGongStatArmorEarth[0][s]);
+                IPlayer.AddOTP(DemonGongStatArmorEarth[1][s]);
+            case 2:
+                IPlayer.IncreaseMaxHp(DemonGongStatArmorWind[0][s]);
+                IPlayer.AddEva(DemonGongStatArmorWind[1][s]);
+            case 3:
+                IPlayer.AddAgi(DemonGongStatArmorWater[0][s]);
+                IPlayer.AddDef(DemonGongStatArmorWater[1][s]);
+        }
+    }
+}
+
+
 void qigongChestStats(int type, int attr, int player, bool remove) {
     IChar IPlayer((void*)player);
      if (remove) {
@@ -87,35 +120,27 @@ int getStat(int grade) {
     if (grade >= 46 && grade <= 49) {
         return 1;
     }
-
     if (grade >= 50 && grade <= 53) {
         return 2;
     }
-
     if (grade >= 55 && grade <= 58) {
         return 3;
     }
-
     if (grade >= 60 && grade <= 63) {
         return 4;
     }
-
     if (grade >= 65 && grade <= 68) {
         return 5;
     }
-
     if (grade >= 70 && grade <= 73) {
         return 6;
     }
-
     if (grade >= 80 && grade <= 83) {
         return 7;
     }
-
     if (grade >= 90 && grade <= 93) {
         return 8;
     }
-
     if (grade >= 95) {
         return 9;
     }
@@ -206,118 +231,90 @@ void __fastcall DefenseApplySpec(int Item, void *edx, int Player)
     IItem IItem((void*)Item);
     IChar IPlayer((void*)Player);
 
-    if (IPlayer.IsOnline())
+    if (!IPlayer.IsOnline())
     {
-        int def = IPlayer.GetDef();
-        CItemDefense::ApplySpec(Item, Player);
-        int check = IPlayer.GetDef() - (def + *((DWORD*)Item + 27));
-        int FirstDemonGongType = 0, SecondDemonGongType = 0;
-        int FirstDemonGongStat = 0, SecondDemonGongStat = 0;
+        return;
+    }
 
-        if (GetItemStat.count(IItem.GetIID()))
+    int def = IPlayer.GetDef();
+    CItemDefense::ApplySpec(Item, Player);
+    int check = IPlayer.GetDef() - (def + *((DWORD*)Item + 27));
+    int FirstDemonGongType = 0, SecondDemonGongType = 0;
+    int FirstDemonGongStat = 0, SecondDemonGongStat = 0;
+
+    if (GetItemStat.count(IItem.GetIID()))
+    {
+        FirstDemonGongType = getItemStat(100000000, 10000000, Item);
+        SecondDemonGongType = getItemStat(1000000, 100000, Item);
+        FirstDemonGongStat = getItemStat(10000000, 1000000, Item);
+        SecondDemonGongStat = getItemStat(100000, 10000, Item);
+    }
+
+    demonGongTypeApply(FirstDemonGongType, FirstDemonGongStat, Player, false);
+    demonGongTypeApply(FirstDemonGongType, SecondDemonGongStat, Player, false);
+
+    if (GetItemStat.find(IItem.GetIID())->second > 0)
+    {
+        int QigongType = GetItemStat.find(IItem.GetIID())->second % 100;
+
+        if (QigongType)
         {
-            // 100000000 = 100.000.000 | 10000000  = 10.000.000
-            FirstDemonGongType = getItemStat(100000000, 10000000, Item);
+            if (IItem.GetType() == 3) {
+                IPlayer.AddDef((check * QigongChest[QigongType - 1][0]) / 100);
+            } else if (IItem.GetType() == 4) {
+                IPlayer.AddDef((check * QigongShort[QigongType - 1][0]) / 100);
+            } else {
+                IPlayer.AddDef((check * QigongMain[QigongType - 1][0]) / 100);
+            }
 
-            // 1000000 = 1.000.000 | 100000  = 100.000
-            SecondDemonGongType = getItemStat(1000000, 100000, Item);
+            int Type = QigongChest[QigongType - 1][1];
+            int Stat = QigongChest[QigongType - 1][2];
+            qigongChestStats(Type, Stat, Player, false);
 
-            // 10000000 = 10.000.000 | 1000000 = 1.000.000)
-            FirstDemonGongStat = getItemStat(10000000, 1000000, Item);
-
-            // 100000 = 100.000 | 10000 = 10.000)
-            SecondDemonGongStat = getItemStat(100000, 10000, Item);
         }
+    }
 
-        switch (FirstDemonGongType)
-        {
-            case 1:
-                IPlayer.AddDef(DemonGongStatArmorEarth[0][FirstDemonGongStat - 1]);
-                IPlayer.AddOTP(DemonGongStatArmorEarth[1][FirstDemonGongStat - 1]);
+    int nItemGST = IItem.GetSetGem() - 200;
+    nItemGSTApply(nItemGST, check, Player, false);
+
+    if (IItem.GetInfo() & 2097152)
+    {
+        int Grade = *(DWORD*)(*(DWORD*)(Item + 40) + 80);
+        int Stat = getStat(Grade);
+
+        playerAddAgiStrInt(Stat, Player);
+
+        setBeadOfFire(Grade, IItem.GetType(), Player)
+
+        switch (IItem.GetType()) {
             case 2:
-                IPlayer.IncreaseMaxHp(DemonGongStatArmorWind[0][FirstDemonGongStat - 1]);
-                IPlayer.AddEva(DemonGongStatArmorWind[1][FirstDemonGongStat - 1]);
+                BeadOfFire[IPlayer.GetPID()].Helmet = Grade;
             case 3:
-                IPlayer.AddAgi(DemonGongStatArmorWater[0][FirstDemonGongStat - 1]);
-                IPlayer.AddDef(DemonGongStatArmorWater[1][FirstDemonGongStat - 1]);
+                BeadOfFire[IPlayer.GetPID()].Chest = Grade;
+            case 4:
+                BeadOfFire[IPlayer.GetPID()].Short = Grade;
+            case 5:
+                BeadOfFire[IPlayer.GetPID()].Gloves = Grade;
+            case 6:
+                BeadOfFire[IPlayer.GetPID()].Boots = Grade;
         }
 
-        switch (SecondDemonGongType)
-        {
-            case 1:
-                IPlayer.AddDef(DemonGongStatArmorEarth[0][SecondDemonGongStat - 1]);
-                IPlayer.AddOTP(DemonGongStatArmorEarth[1][SecondDemonGongStat - 1]);
-            case 2:
-                IPlayer.IncreaseMaxHp(DemonGongStatArmorWind[0][SecondDemonGongStat - 1]);
-                IPlayer.AddEva(DemonGongStatArmorWind[1][SecondDemonGongStat - 1]);
-            case 3:
-                IPlayer.AddAgi(DemonGongStatArmorWater[0][SecondDemonGongStat - 1]);
-                IPlayer.AddDef(DemonGongStatArmorWater[1][SecondDemonGongStat - 1]);
+        if (!BeadOfFire.count(IPlayer.GetPID())) {
+            return;
         }
 
-        if (GetItemStat.find(IItem.GetIID())->second > 0)
-        {
-            int QigongType = GetItemStat.find(IItem.GetIID())->second % 100;
+        CheckBof bof = BeadOfFire.find(IPlayer.GetPID())->second;
 
-            if (QigongType)
-            {
-                if (IItem.GetType() == 3) {
-                    IPlayer.AddDef((check * QigongChest[QigongType - 1][0]) / 100);
-                } else if (IItem.GetType() == 4) {
-                    IPlayer.AddDef((check * QigongShort[QigongType - 1][0]) / 100);
-                } else {
-                    IPlayer.AddDef((check * QigongMain[QigongType - 1][0]) / 100);
-                }
-
-                int Type = QigongChest[QigongType - 1][1];
-                int Stat = QigongChest[QigongType - 1][2];
-
-                qigongChestStats(Type, Stat, Player, false);
-
-            }
-        }
-
-        int nItemGST = IItem.GetSetGem() - 200;
-        nItemGSTApply(nItemGST, check, Player, false);
-
-        if (IItem.GetInfo() & 2097152)
-        {
-            int Grade = *(DWORD*)(*(DWORD*)(Item + 40) + 80);
-            int Stat = getStat(Grade);
-
-            playerAddAgiStrInt(Stat, Player);
-
-            setBeadOfFire(Grade, IItem.GetType(), Player)
-
-            switch (IItem.GetType()) {
-                case 2:
-                    BeadOfFire[IPlayer.GetPID()].Helmet = Grade;
-                case 3:
-                    BeadOfFire[IPlayer.GetPID()].Chest = Grade;
-                case 4:
-                    BeadOfFire[IPlayer.GetPID()].Short = Grade;
-                case 5:
-                    BeadOfFire[IPlayer.GetPID()].Gloves = Grade;
-                case 6:
-                    BeadOfFire[IPlayer.GetPID()].Boots = Grade;
-            }
-
-            if (!BeadOfFire.count(IPlayer.GetPID())) {
-                return;
-            }
-
-            CheckBof bof = BeadOfFire.find(IPlayer.GetPID())->second;
-
-            beadOfFireApply(46, 1, Player, false);
-            beadOfFireApply(50, 2, Player, false);
-            beadOfFireApply(55, 3, Player, false);
-            beadOfFireApply(60, 4, Player, false);
-            beadOfFireApply(65, 5, Player, false);
-            beadOfFireApply(70, 6, Player, false);
-            beadOfFireApply(80, 7, Player, false);
-            beadOfFireApply(90, 8, Player, false);
-            beadOfFireApply(95, 9, Player, false);
-        }
+        beadOfFireApply(46, 1, Player, false);
+        beadOfFireApply(50, 2, Player, false);
+        beadOfFireApply(55, 3, Player, false);
+        beadOfFireApply(60, 4, Player, false);
+        beadOfFireApply(65, 5, Player, false);
+        beadOfFireApply(70, 6, Player, false);
+        beadOfFireApply(80, 7, Player, false);
+        beadOfFireApply(90, 8, Player, false);
+        beadOfFireApply(95, 9, Player, false);
+    }
     }
 }
 
@@ -339,20 +336,14 @@ void __fastcall ArmorPutOn(int Item, void *edx, int Player)
 
     IPlayer.Buff(256, 2, 0);
 
-    if (IPlayer.IsOnline() && IPlayer.IsBuff(120))
-    {
-        if (IItem.LevelLimit() > 5) {
-            IItem.SetLevelLimit(IItem.LevelLimit() - 5);
-        }
+    if (IPlayer.IsOnline() && IPlayer.IsBuff(120) && IItem.LevelLimit() > 5) {
+        IItem.SetLevelLimit(IItem.LevelLimit() - 5);
     }
 
     CItemDefense::PutOn(Item, Player);
 
-    if (IPlayer.IsOnline() && IPlayer.IsBuff(120))
-    {
-        if (IItem.LevelLimit() > 5) {
-            IItem.SetLevelLimit(IItem.LevelLimit() + 5);
-        }
+    if (IPlayer.IsOnline() && IPlayer.IsBuff(120) && IItem.LevelLimit() > 5) {
+        IItem.SetLevelLimit(IItem.LevelLimit() + 5);
     }
 }
 
@@ -379,10 +370,7 @@ void __fastcall DefensePutOff(void *Item, void *edx, int Player)
         int Grade = *(DWORD*)(*(DWORD*)((int)Item + 40) + 80);
         int Stat = getStat(Grade);
 
-        IPlayer.RemoveAgi(Stat);
-        IPlayer.RemoveStr(Stat);
-        IPlayer.RemoveInt(Stat);
-
+        playerRemoveAgiStrInt(Stat, player);
         setBeadOfFire(0, IItem.GetType(), Player)
 
         if (BeadOfFire.count(IPlayer.GetPID())) {
@@ -409,57 +397,14 @@ void __fastcall DefensePutOff(void *Item, void *edx, int Player)
 
     if (GetItemStat.count(IItem.GetIID()))
     {
-        FirstDemonGongType = (GetItemStat.find(IItem.GetIID())->second % 100000000) /
-            10000000;
-        SecondDemonGongType = (GetItemStat.find(IItem.GetIID())->second % 1000000) /
-            100000;
-        FirstDemonGongStat = (GetItemStat.find(IItem.GetIID())->second % 10000000) /
-            1000000;
-        SecondDemonGongStat = (GetItemStat.find(IItem.GetIID())->second % 100000) /
-            10000;
+        FirstDemonGongType = getItemStat(100000000, 10000000, Item);
+        SecondDemonGongType = getItemStat(1000000, 100000, Item);
+        FirstDemonGongStat = getItemStat(10000000, 1000000, Item);
+        SecondDemonGongStat = getItemStat(100000, 10000, Item);
     }
 
-    if (FirstDemonGongType)
-    {
-        if (FirstDemonGongType == 1)
-        {
-            IPlayer.RemoveDef(DemonGongStatArmorEarth[0][FirstDemonGongStat - 1]);
-            IPlayer.RemoveOTP(DemonGongStatArmorEarth[1][FirstDemonGongStat - 1]);
-        }
-
-        if (FirstDemonGongType == 2)
-        {
-            IPlayer.DecreaseMaxHp(DemonGongStatArmorWind[0][FirstDemonGongStat - 1]);
-            IPlayer.RemoveEva(DemonGongStatArmorWind[1][FirstDemonGongStat - 1]);
-        }
-
-        if (FirstDemonGongType == 3)
-        {
-            IPlayer.RemoveAgi(DemonGongStatArmorWater[0][FirstDemonGongStat - 1]);
-            IPlayer.RemoveDef(DemonGongStatArmorWater[1][FirstDemonGongStat - 1]);
-        }
-    }
-
-    if (SecondDemonGongType)
-    {
-        if (SecondDemonGongType == 1)
-        {
-            IPlayer.RemoveDef(DemonGongStatArmorEarth[0][SecondDemonGongStat - 1]);
-            IPlayer.RemoveOTP(DemonGongStatArmorEarth[1][SecondDemonGongStat - 1]);
-        }
-
-        if (SecondDemonGongType == 2)
-        {
-            IPlayer.DecreaseMaxHp(DemonGongStatArmorWind[0][SecondDemonGongStat - 1]);
-            IPlayer.RemoveEva(DemonGongStatArmorWind[1][SecondDemonGongStat - 1]);
-        }
-
-        if (SecondDemonGongType == 3)
-        {
-            IPlayer.RemoveAgi(DemonGongStatArmorWater[0][SecondDemonGongStat - 1]);
-            IPlayer.RemoveDef(DemonGongStatArmorWater[1][SecondDemonGongStat - 1]);
-        }
-    }
+    demonGongTypeApply(FirstDemonGongType, FirstDemonGongStat, Player, true);
+    demonGongTypeApply(FirstDemonGongType, SecondDemonGongStat, Player, true);
 
     if (GetItemStat.find(IItem.GetIID())->second > 0)
     {
@@ -467,25 +412,18 @@ void __fastcall DefensePutOff(void *Item, void *edx, int Player)
 
         if (QigongType)
         {
-            if (IItem.GetType() == 3)
-            {
-                IPlayer.RemoveDef((check * QigongChest[QigongType - 1][0]) / 100);
-                int Type = QigongChest[QigongType - 1][1];
-                int Stat = QigongChest[QigongType - 1][2];
-                qigongChestStats(Type, Stat, Player, true);
+            switch (IItem.GetType()) {
+                case 3:
+                    IPlayer.RemoveDef((check * QigongChest[QigongType - 1][0]) / 100);
+                case 4:
+                    IPlayer.RemoveDef((check * QigongShort[QigongType - 1][0]) / 100);
+                default:
+                    IPlayer.RemoveDef((check * QigongMain[QigongType - 1][0]) / 100);
 
-            } else if (IItem.GetType() == 4)
-            {
-                IPlayer.RemoveDef((check * QigongShort[QigongType - 1][0]) / 100);
-                int Type = QigongChest[QigongType - 1][1];
-                int Stat = QigongChest[QigongType - 1][2];
-                qigongChestStats(Type, Stat, Player, true);
-            } else {
-                IPlayer.RemoveDef((check * QigongMain[QigongType - 1][0]) / 100);
-                int Type = QigongChest[QigongType - 1][1];
-                int Stat = QigongChest[QigongType - 1][2];
-                qigongChestStats(Type, Stat, Player, true);
             }
+            int Type = QigongChest[QigongType - 1][1];
+            int Stat = QigongChest[QigongType - 1][2];
+            qigongChestStats(Type, Stat, Player, true);
         }
     }
 
